b21fc691ce578fe05553cd76fe8eaa7022aacc10
Daniel Webb <danwebb94@gmail.com>
2016-10-31 17:36:23 +0200

    added web deploy, requries refactor still

--- build/build.ps1
+++ build/build.ps1
@@ -1,12 +1,13 @@
 Framework "4.6"
 properties {
     $solutionPath
+    $configuration
 }
 
 task default -depends Build
 
 task Build -depends StartClean{
-    Exec {MsBuild "$solutionPath"} 
+    Exec {MsBuild "$solutionPath" /p:Configuration=$configuration} 
 }
 
 task StartClean {
--- build/deploylocal.ps1
+++ build/deploylocal.ps1
@@ -1,15 +1,50 @@
+Framework "4.6"
+Import-Module '.\psake-master\psake.psm1'
+
 properties {
-    $newAppPool = ""
     $appPoolName = "nancysake"
+    $deployDir = "C:\Websites"
+
+  $msDeployLocation = "C:\Program Files (x86)\IIS\Microsoft Web Deploy V3\"
+  $configuration = "Debug"
+  $deploy = @{
+    "Debug" = @{
+      "PublishProfileName" = "test";
+      "BinaryLocation" = "bin\Debug";
+      "ServerName" = "localhost";
+      "SiteName" = "nancyTest";
+      "ServiceFolder" = "C:\Websites";
+        };
+    }
 }
+
 
 task default -depends DeployLocal
 
-task DeployLocal -Depends CreateAppPool {
+task DeployLocal -depends CreateAppPool {
     Write-Host 'Creating new website'
-    New-Website -name $appPoolName -PhysicalPath $deploy_dir -ApplicationPool $appPoolName -HostHeader $deploy_Url
+    # To Be Refactored
+    # New-Item -ItemType Directory -Path $deployDir
+    New-Website -Name 'nancysake' -Port 9090 -IPAddress 127.0.0.1 -PhysicalPath $deployDir -ApplicationPool $appPoolName
+    exec { msbuild "C:\_dev\nancy-build\nancysake\nancysake\nancysake.csproj" /p:AspnetCompilerPath="C:\windows\Microsoft.NET\Framework\v4.0.30319" /p:DeployOnBuild=true /p:PublishProfile="$($deploy[$configuration].PublishProfileName)" /p:Configuration=$configuration}
+
+}
+
+task Test -depends Build {
+    $unitTestProject = Get-ChildItem -Filter "*Tests" -Directory "$PSScriptRoot\..\nancysake"
+
+    $unitTestProjects | %{
+    $assemblyPath = "$PSScriptRoot\..\nancysake\\$_\$($deploy[$configuration].BinaryLocation)\$_.dll"
+      exec {& $testExecutable /testcontainer:$assemblyPath /category:Unit }
+  }
+ 
+  Remove-Item $PSScriptRoot\TestResults -Recurse
+}
+
+task Build {
+    Invoke-psake 'build.ps1' -parameters @{'solutionPath' = '..\nancysake\nancysake.sln'}
 }
 
 task CreateAppPool {
-    Invoke-psake 'create-app-pool.ps1' -parameters @{'appPoolName' = $appPoolName; 'newAppPool' = $newAppPool}
+    Invoke-psake 'create-app-pool.ps1' -parameters @{'appPoolName' = $appPoolName;}
 }
